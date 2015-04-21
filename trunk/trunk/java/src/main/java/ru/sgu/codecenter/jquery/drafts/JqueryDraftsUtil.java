a188037c847edc3c151b5837d2dce7e4a285d568
sladethe <sladethe@8856e477-b242-d303-133a-b4b139f59bf8>
2015-04-21 12:03:40 +0000

    - fixed minor UI bug; - updated timezone; - few minor improvements.

--- trunk/trunk/java/src/main/java/ru/sgu/codecenter/jquery/drafts/JqueryDraftsUtil.java
+++ trunk/trunk/java/src/main/java/ru/sgu/codecenter/jquery/drafts/JqueryDraftsUtil.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class JqueryDraftsUtil {
     public static List<String> update(List<String> items, int maxSize, String text) {
         if (items == null) {
-            items = new ArrayList<String>(1);
+            items = new ArrayList<>(1);
         }
 
         if (text == null || text.length() <= 1) {
@@ -19,7 +19,7 @@ public class JqueryDraftsUtil {
             int bestIndex = -1;
             int bestDifference = Integer.MAX_VALUE;
 
-            for (int i = 0; i < items.size(); i++) {
+            for (int i = 0; i < items.size(); ++i) {
                 int currentDifference = difference(items.get(i), text);
                 if (currentDifference < bestDifference) {
                     bestDifference = currentDifference;
@@ -42,16 +42,16 @@ public class JqueryDraftsUtil {
     }
 
     private static int difference(String s, String t) {
-        int l = 0;
-        while (l < s.length() && l < t.length() && s.charAt(l) == t.charAt(l)) {
-            l++;
+        int left = 0;
+        while (left < s.length() && left < t.length() && s.charAt(left) == t.charAt(left)) {
+            ++left;
         }
 
-        int r = 0;
-        while (r < s.length() && r < t.length() && s.charAt(s.length() - 1 - r) == t.charAt(t.length() - 1 - r)) {
-            r++;
+        int right = 0;
+        while (right < s.length() && right < t.length() && s.charAt(s.length() - 1 - right) == t.charAt(t.length() - 1 - right)) {
+            ++right;
         }
 
-        return Math.max(0, s.length() - l - r) + Math.max(0, t.length() - l - r);
+        return StrictMath.max(0, s.length() - left - right) + StrictMath.max(0, t.length() - left - right);
     }
 }
