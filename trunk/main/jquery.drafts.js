827f84f2d70489030e83647f9497c247a83f31b5
MikeMirzayanov <mirzayanovmr@gmail.com>
2021-10-26 17:32:06 +0300

    Synchronized with Polygon

--- trunk/main/jquery.drafts.js
+++ trunk/main/jquery.drafts.js
@@ -1,13 +1,15 @@
 (function ($) {
     var textareasToListen = [];
     var buttonsToListen = [];
-    var alertedOnFail = false;
-    var consecutiveFailCount = 0;
+    var textById = {};
 
     var updateButtonsPosition = function () {
         $.each(textareasToListen, function (index, textarea) {
             var button = buttonsToListen[index];
             var buttonContainer = textarea;
+            if (buttonContainer.is(":hidden")) {
+                button.hide();
+            }
 
             var buttonContainerSelector = button.attr("buttonContainerSelector");
             if (buttonContainerSelector && buttonContainerSelector.length > 0) {
@@ -29,6 +31,9 @@
 
             button.css("top", buttonContainer.position().top + yOffset);
             button.css("left", buttonContainer.position().left + xOffset);
+            if (!buttonContainer.is(":hidden")) {
+                button.show();
+            }
         });
     };
 
@@ -45,29 +50,48 @@
         popup.css("top", pageYOffset);
     }
 
+    function _escapeHtml(s) {
+        return s
+            .replace(/&/g, "&amp;")
+            .replace(/</g, "&lt;")
+            .replace(/>/g, "&gt;")
+            .replace(/"/g, "&quot;")
+            .replace(/'/g, "&#039;");
+    }
+
     function addDraftEntry(item, settings) {
         var popup = $(".drafts-popup");
         var entries = popup.find(".drafts-entries");
         var hasSame = false;
-        entries.find(".drafts-entry-body").each(function() {
-            if ($(this).text() == item) {
+        entries.find(".drafts-entry-body").each(function () {
+            if ($(this).text() === item.draft) {
                 hasSame = true;
                 return false;
             }
         });
+        let time = "";
+        if (item.time !== "") {
+            time = new Date(item.time).toLocaleString();
+        }
         if (!hasSame) {
-            $("<pre class='drafts-entry-body'></pre>").text(item).appendTo(entries);
-            $("<div class='drafts-entry-actions'><span class='drafts-button'>" + settings["textUseIt"] + "</span></div>").appendTo(entries);
+            $("<pre class='drafts-entry-body'></pre>").text(item.draft).appendTo(entries);
+            $("<div class='drafts-entry-actions' style='display: inline'><span class='drafts-button'>" + settings["textUseIt"] + "</span></div><span class='drafts-creation-time'> " + _escapeHtml(time) + "</span>").appendTo(entries);
         }
     }
 
     function addFromLocalStorage(publicKey, key, settings) {
         if (publicKey && key) {
             var text = window.localStorage["draft-" + publicKey];
+            let time = window.localStorage["draft-time-" + publicKey];
             if (text) {
                 try {
                     text = sjcl.decrypt(key, text);
-                    addDraftEntry(text, settings);
+                    if (time) {
+                        time = sjcl.decrypt(key, time);
+                    } else {
+                        time = "";
+                    }
+                    addDraftEntry({draft: text, time: time}, settings);
                 } catch (e) {
                     window.localStorage.removeItem("draft-" + publicKey);
                 }
@@ -75,10 +99,13 @@
         }
     }
 
-    function putToLocalStorage(publicKey, key, text) {
+    function putToLocalStorage(publicKey, key, text, time) {
         if (publicKey && key && text) {
             if (window.localStorage) {
                 window.localStorage["draft-" + publicKey] = sjcl.encrypt(key, text);
+                if (time) {
+                    window.localStorage["draft-time-" + publicKey] = sjcl.encrypt(key, time);
+                }
             }
         }
     }
@@ -88,25 +115,21 @@
             textDrafts: 'Drafts',
             textUseIt: 'Use it',
             url: '/data/drafts.php',
-            pollDelay: 5000,
+            pollDelay: 10000,
             showDelay: 2500,
             saveErrorMessage: "Can't save draft. Possibly connection is lost or session is expired. Stay on the page (cancel to reload)?",
             useItHandler: function (textarea, text) {
                 textarea.val(text);
+                textarea.trigger("paste.autoResize");
             }
         });
-    
-        $.extend(settings, {
-            saveErrorHandler: function () {
-                if (!confirm(settings["saveErrorMessage"])) {
-                    location.reload();
-                }
-            }
-        });
-        
+
         $.extend(settings, options);
 
         var textareas = this;
+        var hasDraftsPrototype = false;
+        var hasDraftsPopup = false;
+
         setTimeout(function () {
             textareas.each(function () {
                 var textarea = $(this);
@@ -114,19 +137,21 @@
                     return;
                 }
 
-                if ($(".drafts-prototype").length === 0) {
+                if (!hasDraftsPrototype && $(".drafts-prototype").length === 0) {
                     $("<div class=\"drafts-prototype drafts-show-drafts\"><span class=\"drafts-button drafts-online\">"
                         + settings["textDrafts"]
                         + "</span></div>").appendTo($("body"));
+                    hasDraftsPrototype = true;
                 }
 
-                if ($(".drafts-popup").length === 0) {
+                if (!hasDraftsPopup && $(".drafts-popup").length === 0) {
                     $("<div class=\"drafts-popup\"><div class=\"drafts-close\">&times;</div><h1>"
                         + settings["textDrafts"]
                         + "</h1><div class=\"drafts-entries\"></div></div>").appendTo($("body"));
+                    hasDraftsPopup = true;
                 }
 
-                if (textarea.prop("tagName").toLowerCase() != "textarea") {
+                if (textarea.prop("tagName").toLowerCase() !== "textarea") {
                     $.error("jquery.drafts.js can be used only for textareas, but " + textarea.prop("tagName").toLowerCase() + " found.");
                 }
 
@@ -135,29 +160,39 @@
                 }
 
                 var id = textarea.attr("data-drafts-id");
-
                 var publicKey;
                 var key;
 
                 var textChanged = false;
-                window.setTimeout(function() {
-                    textarea.change(function() {
+                window.setTimeout(function () {
+                    textarea.change(function () {
                         textChanged = true;
                     });
-                    textarea.bind('input propertychange', function() {
+                    textarea.bind('input propertychange', function () {
                         textChanged = true;
                     });
-                }, 60000);
+                }, 10000);
 
-                window.setInterval(function() {
+                window.setInterval(function () {
                     if (!key) {
                         $.post(settings["url"], {action: 'getKey', id: id}, function (result) {
                             publicKey = result["publicKey"];
                             key = result["key"];
+                            let text = window.localStorage["draft-" + publicKey];
+                            let time = window.localStorage["draft-time-" + publicKey];
+                            if (text) {
+                                text = sjcl.decrypt(key, text);
+                                if (time) {
+                                    time = sjcl.decrypt(key, time);
+                                } else {
+                                    time = new Date().toISOString();
+                                }
+                                $.post(settings["url"], {action: 'put', id: id, text: text, time: time});
+                            }
                         }, "json");
                     }
                     if (key && textChanged) {
-                        putToLocalStorage(publicKey, key, textarea.val());
+                        putToLocalStorage(publicKey, key, textarea.val(), new Date().toISOString());
                         textChanged = false;
                     }
                 }, 1000);
@@ -177,16 +212,52 @@
                 button.click(function () {
                     var popup = $(".drafts-popup");
                     var entries = popup.find(".drafts-entries");
-                    //entries.empty();
-                    //addFromLocalStorage(id, settings);
-                    //showDraftsPopup(settings);
+                    entries.empty();
+                    addFromLocalStorage(publicKey, key, settings);
+                    showDraftsPopup(textarea, settings);
 
                     $.post(settings["url"], {action: 'get', id: id}, function (items) {
+                        // entries.empty();
+                        // addFromLocalStorage(publicKey, key, settings);
+
                         entries.empty();
-                        addFromLocalStorage(publicKey, key, settings);
-                        $.each(items, function (index, item) {
-                            addDraftEntry(item, settings);
+                        let localDraft = undefined;
+                        if (publicKey) {
+                            let text = window.localStorage["draft-" + publicKey];
+                            let time = window.localStorage["draft-time-" + publicKey];
+                            if (key && text) {
+                                text = sjcl.decrypt(key, text);
+                                if (time) {
+                                    time = sjcl.decrypt(key, time);
+                                } else {
+                                    time = "";
+                                }
+                                localDraft = {draft: text, time: time};
+                            }
+                        }
+
+                        let draftsAndTimes = [];
+                        if (localDraft) {
+                            draftsAndTimes.push(localDraft);
+                        }
+                        $.each(items, function (draft, time) {
+                            draftsAndTimes.push({
+                                draft: draft,
+                                time: time
+                            });
+                        })
+                        draftsAndTimes.sort(function (lhs, rhs) {
+                            if (lhs.time === "" && rhs.time === "") {
+                                return 0;
+                            } else if (lhs.time === "") {
+                                return 1;
+                            } else if (rhs.time === "") {
+                                return -1;
+                            } else {
+                                return Date.parse(rhs.time) - Date.parse(lhs.time);
+                            }
                         });
+                        draftsAndTimes.forEach(element => addDraftEntry(element, settings));
                         showDraftsPopup(textarea, settings);
                     }, "json");
                 });
@@ -218,39 +289,37 @@
                 $(".drafts-popup .drafts-close").click(function () {
                     $(".drafts-popup").fadeOut();
                 });
+                $(document).keydown(function(e) {
+                    if (e.keyCode === 27) {
+                        $(".drafts-popup").fadeOut();
+                    }
+                });
 
                 window.setInterval(function () {
                     var text = textarea.val();
                     var innerButton = button.find(".drafts-button");
-                    $.post(settings["url"], {action: 'put', id: id, text: text}, function (response) {
+                    if (textById[id] === text) {
+                        return;
+                    }
+                    textById[id] = text;
+                    $.post(settings["url"], {action: 'put', id: id, text: text, time: new Date().toISOString()}, function (response) {
                         if (response === "OK") {
-                            consecutiveFailCount = 0;
                             if (!innerButton.hasClass("drafts-online")) {
                                 innerButton.removeClass("drafts-offline");
                                 innerButton.addClass("drafts-online");
                             }
                         } else {
-                            ++consecutiveFailCount;
                             if (!innerButton.hasClass("drafts-offline")) {
                                 innerButton.removeClass("drafts-online");
                                 innerButton.addClass("drafts-offline");
-                            }
-                            if (consecutiveFailCount >= 5 * textareasToListen.length && !alertedOnFail) {
-                                alertedOnFail = true;
-                                settings["saveErrorHandler"]();
                             }
                         }
                     }, "json").fail(function () {
-                            ++consecutiveFailCount;
-                            if (!innerButton.hasClass("drafts-offline")) {
-                                innerButton.removeClass("drafts-online");
-                                innerButton.addClass("drafts-offline");
-                            }
-                            if (consecutiveFailCount >= 5 * textareasToListen.length && !alertedOnFail) {
-                                alertedOnFail = true;
-                                settings["saveErrorHandler"]();
-                            }
-                        });
+                        if (!innerButton.hasClass("drafts-offline")) {
+                            innerButton.removeClass("drafts-online");
+                            innerButton.addClass("drafts-offline");
+                        }
+                    });
                 }, settings["pollDelay"]);
             });
         }, settings["showDelay"]);
